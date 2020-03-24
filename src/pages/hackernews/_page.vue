afe0523d51ae8658accbb988c4a3cd4e1895107a
tofu511 <mokmok511@gmail.com>
2020-03-24 23:47:48 +0900

    Feat: [WIP] fetch hacker news data using asyncData function

--- src/pages/hackernews/_page.vue
+++ src/pages/hackernews/_page.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>This is _page.vue</div>
+  <div>
+    <pre>{{ result }}</pre>
+  </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  async asyncData (context):Promise<Object> {
+    const res = await context.$axios.$get(`https://api.hnpwa.com/v0/${context.params.page}/1.json`)
+    return { result: res }
+  }
+})
+
+export default class Page extends Vue {
+  // result: Object = {}
+}
+</script>
